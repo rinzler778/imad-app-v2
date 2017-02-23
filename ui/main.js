5937736bc1482f3829c732be3531c440e998d6cb
rinzler778 <ajithphilip255@gmail.com>
2017-02-23 22:54:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-  //make request to server and send the name
-  
+   
+     //1
+    var request = new XMLHttpRequest();
+    
+        //2
+request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        
+        if(request.status === 200) {
  //capture names and render it as a list
- var names = ['name1', 'name2', 'name3', 'name4'];
+ var names = request.responseText;
+ names = JSON.parse(names);
  var list = '';
  for (var i=0; i< names.length; i++) {
      list+= '<li>' + names[i] + '</li>';
  }
  var ul = document.getElementById('namelist');
  ul.innerHTML = list;
+        }
+    }
+    
+};
+
+//3
+request.open('GET', 'http://rinzler778.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+  //make request to server and send the name
+  
+
 };
