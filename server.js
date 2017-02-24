56e6a912c2d26fd47e5267ec9d0461740d43b9e5
rinzler778 <ajithphilip255@gmail.com>
2017-02-24 15:14:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'rinzler778',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: 'iamrinzler778'
 };
 
 var app = express();
