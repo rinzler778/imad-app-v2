370f065f9656df55da5468f8a7a120670bad49d0
rinzler778 <ajithphilip255@gmail.com>
2017-02-24 12:46:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'rinzler778',
+    database: 'rinzler778',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB.PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,6 +90,12 @@ return htmlTemplate;
 
 app.get('/', function (req, res) { 
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req,res) {
+    
+    
 });
 
 var counter = 0;
