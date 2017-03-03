282c430d6350cc7a87c6a60a3443c71440db426e
rinzler778 <ajithphilip255@gmail.com>
2017-03-03 21:30:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,6 +125,7 @@ app.get('/articles/:articleName', function (req,res) {
    //
   //
  
+ //
  pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName + "'", function (err, result){
      if (err){
          res.sattus(500).send(err.toString());
@@ -132,8 +133,8 @@ app.get('/articles/:articleName', function (req,res) {
          if (result.rows.length === 0) {
              res.status(404).send('Article not found');
          } else {
-             var articleData = resu;t.rows[0];
-             res.send(createTemplate(articles[articleName]));
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
          }
      }
  });
